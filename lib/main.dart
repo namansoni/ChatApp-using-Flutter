7842dda7af5f243c0a622063687f01cfdb11f6e5
namansoni <soninaman62626@gmail.com>
2020-08-09 11:38:48 +0530

    comment removed

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'Services/authService.dart';
-//This is where the app starts (main)
+
 void main(){
   runApp(MyApp());
 }
